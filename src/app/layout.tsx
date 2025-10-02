9a1353db6ee3cdb28ee7ba1e4310351d3ec936d0
DataBase137 <dev.database137@outlook.com>
2025-10-02 09:32:23 -0700

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 import { Poppins } from "next/font/google"
 import { Inter } from "next/font/google"
+import Head from "next/head"
 
 export const metadata: Metadata = {
   title: "chatty",
@@ -18,6 +19,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="chatty" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className={`${poppins.className} ${inter.className}`}>
         {children}
       </body>
